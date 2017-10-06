0ce9d1b2b7ec0657eb8b39e9c2a79a792b5e9dd1
bpo106 <bpo106bkv@gmail.com>
2017-10-06 10:09:18 +0200

    All three tests passed

--- uniquechars/UniqueChars/UniqueChars/UniqueCharacter.cs
+++ uniquechars/UniqueChars/UniqueChars/UniqueCharacter.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace UniqueChars
 {
-    public class UniqueChars
+    public class UniqueCharacter
     {
         public static void Main(string[] args)
         {
@@ -11,6 +12,38 @@ namespace UniqueChars
             // Create basic unit tests for it with at least 3 different test cases
             // Print the characters from that list in the following format:
             // "n", "g", "r", "m"
+        }
+
+        public List<string> UniqueCharacters (string word)
+        {
+            var list = new List<string>();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                list.Add(word.ToCharArray()[i].ToString());
+            }
+
+            for (int i = 0; i < list.Count;)
+            {
+                int count = list.Count;
+                for (int j = i; j < list.Count; j++)
+                {
+                    if (list[i] == list[j] && j != i)
+                    {
+                        list.RemoveAt(j);
+                    }
+                }
+                if (count > list.Count)
+                {
+                    list.RemoveAt(i);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return list;
         }
     }
 }
--- uniquechars/UniqueChars/UniqueCharsTest/UniqueCharsTest.cs
+++ uniquechars/UniqueChars/UniqueCharsTest/UniqueCharsTest.cs
@@ -4,11 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using UniqueChars;
 
 namespace UniqueCharsTest
 {
     [TestFixture]
     public class UniqueCharsTest
     {
+        [Test]
+        public void TestNoUniqueChars ()
+        {
+            var uniquechars = new UniqueCharacter();
+            var list = new List<string>();
+            Assert.AreEqual(list, uniquechars.UniqueCharacters("meme"));
+        }
+
+        [Test]
+        public void TestAllUniqueChars()
+        {
+            var uniquechars = new UniqueCharacter();
+            var list = new List<string>{ "s", "a", "l", "t" };
+            Assert.AreEqual(list, uniquechars.UniqueCharacters("salt"));
+        }
+
+        [Test]
+        public void TestSomeUniqueChars()
+        {
+            var uniquechars = new UniqueCharacter();
+            var list = new List<string> { "a", "r", "e" };
+            Assert.AreEqual(list, uniquechars.UniqueCharacters("address"));
+        }
     }
 }
